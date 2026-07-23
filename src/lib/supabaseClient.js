96fddf1e29eb00755179c65015f98d32b79fb384
viniimansz <viniimansz@gmail.com>
2026-07-23 18:43:57 -0300

    Delete src directory

--- src/lib/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const SUPABASE_URL = 'https://xxybrqbiozggnbkbboft.supabase.co';
-export const SUPABASE_KEY = 'sb_publishable_NIa6sbIhDtai09QlIH3HHQ_n6BdnTXl';
-export const GUARDIAN_EMAIL = 'viniimansz@gmail.com';
-export const MAX_CHARACTERS = 10;
-
-export const sb = createClient(SUPABASE_URL, SUPABASE_KEY);
